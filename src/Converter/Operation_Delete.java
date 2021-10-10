bbf12b424b31faec687e917220eeddb229c9d89d
Eddy-123 <adegnandjoueddy12@gmail.com>
2021-10-10 23:29:40 +0100

    basic update convertor and conversion design

---
+++ src/Converter/Operation_Delete.java
@@ -0,0 +1,17 @@
+package Converter;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Operation_Delete {
+	String sql = "";
+	public Operation_Delete(String sql) {
+		this.sql = sql;
+	}
+	public String convertir_en_json() {
+		String sql = this.sql;
+		String json = "";
+		
+		return json;
+	}
+}
